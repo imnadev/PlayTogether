c25e689d6aef610a6a9e5abf8b4353e0806531cc
imandev <imnadev@gmail.com>
2021-12-01 17:19:30 +0500

    Defined vlc.h functions

--- vlc/vlc.h
+++ vlc/vlc.h
@@ -1,4 +1,22 @@
 #ifndef PLAYTOGETHER_VLC_H
 #define PLAYTOGETHER_VLC_H
 
+#include <stdbool.h>
+
+void vlc_is_available(void (*vlc_is_available_callback(bool))); // Check if vlc is available and send result to callback
+
+void vlc_is_open(void (*vlc_is_open_callback(bool))); // Check if vlc is open and send result to callback
+
+void vlc_play();
+
+void vlc_pause();
+
+void vlc_seek_forward(); //Seek 10 seconds forward
+
+void vlc_seek_backward(); //Seek 10 seconds backward
+
+void vlc_get_seek_position(void (*vlc_get_seek_position_callback(int))); //Get current video position in seconds
+
+void vlc_set_seek_position(int); //Set current video position to given seconds
+
 #endif
